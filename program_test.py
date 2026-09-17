4c03a6d36af74e82961cded040994ef8d7139649
Trevor654 <129467145+Trevor654@users.noreply.github.com>
2026-09-17 08:48:19 -0600

    Added the program_test.py file to have a dedicated testing file using pytest. Right now only tests the halt function

---
+++ program_test.py
@@ -0,0 +1,21 @@
+import pytest
+from main import CPU
+
+
+testing_CPU = CPU('Testing_files/branchtestfile.txt')
+# testing_CPU.run()
+print(testing_CPU._CPU__accumulator)
+
+def test_branch():
+    pass
+
+def test_branchneg():
+    pass
+
+def test_branchzero():
+    pass
+
+def test_halt():
+    assert testing_CPU._CPU__HALT('43') == True
+    assert testing_CPU._CPU__HALT('44') == False
+
