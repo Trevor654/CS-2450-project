9a941ba253d1e6a15a0504fb582ab5380ad87ab6
ESanchez938 <enoch.s938@gmail.com>
2026-09-17 15:44:06 -0600

    Update program_test.py
    adding test cases to test_init_success to make sure the memory allocates things correctly

--- program_test.py
+++ program_test.py
@@ -22,10 +22,21 @@ def test_init_invalid_string_in_file():
         CPU1 = CPU('Testing_files/test_bad_string_2.txt')
 
 
-def test_initialization_success():
+def test_init_success():
     # Testing to see that no errors are raised by inputing valid testFileNames
     CPU1 = CPU('Testing_files/Test1.txt')
     CPU2 = CPU('Testing_files/Test2.txt')
+    #testing to see that all of the memory allocations are correct
+    assert CPU1.memory[0] == '+1007'
+    assert CPU1.memory[1] == '+1008'
+    assert CPU1.memory[2] == '+2007'
+    assert CPU1.memory[3] == '+3008'
+    assert CPU1.memory[4] == '+2109'
+    assert CPU1.memory.get(5) == '+1109'
+    assert CPU1.memory.get(6) == '+4300'
+    assert CPU1.memory.get(7) == '+0000'
+    assert CPU1.memory.get(8) == '+0000'
+    assert CPU1.memory.get(9) == '+0000'
 
 def test_branch():
     pass
